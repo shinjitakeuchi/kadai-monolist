42bdbafb800a1268d1b09354e6a9c9a528917033
conquering lion <skycap2017@tutanota.com>
2017-08-03 03:56:19 +0000

    have views but not functions

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -8,9 +8,16 @@ class OwnershipsController < ApplicationController
       @item.save
     end
     
+    # Want関係として保存
     if params[:type] == 'Want'
        current_user.want(@item)
-       flash[:success] = '商品を Want しました。'
+       flash[:success] = '商品をWantしました。'
+    end
+    
+    # Have関係として保存
+    if params[:type] == 'Have'
+       current_user.have(@item)
+       flash[:success] = '商品をHaveしました。'
     end
     
     redirect_back(fallback_location: root_path)
@@ -19,9 +26,16 @@ class OwnershipsController < ApplicationController
   def destroy
     @item = Item.find(params[:item_id])
     
+    # Want関係を解除
     if params[:type] == 'Want'
       current_user.unwant(@item) 
-      flash[:success] = '商品の Want を解除しました。'
+      flash[:success] = '商品のWantを解除しました。'
+    end
+    
+    # Have関係を解除
+    if params[:type] == 'Have'
+      current_user.unhave(@item) 
+      flash[:success] = '商品のHaveを解除しました。'
     end
     
     redirect_back(fallback_location: root_path)
